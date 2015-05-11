edce156cb99436f80d4487c8287f811b755ddbdf
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2015-05-11 09:31:33 +0200

    scripts: update boot and installation scripts

--- scripts/boot_up_scripts/raspbian_first_boot.sh
+++ scripts/boot_up_scripts/raspbian_first_boot.sh
@@ -8,7 +8,8 @@
 
 ### Install required packages (netlink dev, generic netlink dev)
 apt-get update
-apt-get install libnl-3-dev libnl-genl-3-dev
+apt-get install bison flex autoconf libtool help2man libnl-3-dev \
+	libnl-genl-3-dev
 
 if [ ! -d "wpan-tools" ]; then
 	echo "[INFO]"
